5f578189a19c069fa01052bb1377d907e5ab4545
Joyal-George-KJ <joyalgeorgekj@gmail.com>
2025-04-13 13:25:40 +0530

    UI improved Layout done

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -4,7 +4,7 @@ import Navbar from "../components/Navbar";
 
 function Layout() {
     return (
-        <div className="max-w-screen min-h-screen bg-neutral-300">
+        <div className="max-w-screen min-h-screen bg-gradient-to-br from-blue-100 via-white to-pink-100 dark:from-neutral-900 dark:via-neutral-800 dark:to-neutral-900">
             <Navbar />
             <div className="px-4 h-4/6 *:text-neutral-800">
                 <Outlet></Outlet>

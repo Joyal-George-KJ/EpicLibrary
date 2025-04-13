046514e13e2047a846486dab008f274f8ef6d7f2
Joyal-George-KJ <joyalgeorgekj@gmail.com>
2025-04-13 13:26:17 +0530

    Loading done

---
+++ src/components/Loader.jsx
@@ -0,0 +1,8 @@
+export default function Loader() {
+    return (
+      <div className="flex items-center justify-center h-[50vh] text-blue-500 text-xl font-semibold animate-pulse">
+        Loading content...
+      </div>
+    );
+  }
+  

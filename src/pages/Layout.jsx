6a7cfc9a05bc2a58f9773205977317b9cf9d396c
Joyal-George-KJ <joyalgeorgekj@gmail.com>
2025-04-16 13:14:31 +0530

    Universal color removed

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -6,7 +6,7 @@ function Layout() {
     return (
         <div className="max-w-screen min-h-screen bg-gradient-to-br from-blue-100 via-white to-pink-100 dark:from-neutral-900 dark:via-neutral-800 dark:to-neutral-900">
             <Navbar />
-            <div className="px-4 h-4/6 *:text-neutral-800">
+            <div className="px-4 h-4/6">
                 <Outlet></Outlet>
             </div>
         </div>
